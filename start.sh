1324b53ecb2706254c396b655586632fbb6b9a74
XU Li <xuenru@gmail.com>
2021-06-05 17:04:36 +0200

    constom config

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#docker run \
+#	--name ipsec-vpn-server \
+#	--env-file ./vpn.env \
+#	--restart=always \
+#	-v ./ikev2-vpn-data:/etc/ipsec.d \
+#	-p 500:500/udp \
+#	-p 4500:4500/udp \
+#	-d --privileged \
+#	ipsec-vpn-server:latest
+
+docker-compose up -d
